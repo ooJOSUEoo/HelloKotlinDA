a6921b76ad63dc791d222f7aff52516ad04994bb
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-10-27 21:04:10 -0500

    Acceder a la vista desde Kotlin

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.hellokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var message: String = findViewById<TextView>(R.id.textView).text.toString()
+        Log.i("message textView",message)
+
     }
 }
