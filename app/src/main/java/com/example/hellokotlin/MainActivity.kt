2eb39c21d3bd599ea35ed2b566a7ba470136f97d
Josue Muñoz Avila <sjosue5082002@gmail.com>
2021-10-27 21:11:39 -0500

    Configurar TextToSpeach

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -2,16 +2,30 @@ package com.example.hellokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    var tts: TextToSpeech? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        tts = TextToSpeech(this, this)
+
         var message: String = findViewById<TextView>(R.id.textView).text.toString()
         Log.i("message textView",message)
 
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS){
+            findViewById<TextView>(R.id.textView).text = "Listo!"
+        }else{
+            findViewById<TextView>(R.id.textView).text = "No disponible :("
+        }
     }
 }
